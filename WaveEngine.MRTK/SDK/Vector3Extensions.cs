0019ca28f3349c8fe3640c0638b5530da013fe4f
Manuel Caballero Sánchez <minolo3ds@gmail.com>
2020-02-12 13:34:18 +0100

    Added Vector3.Project extension method until it is available in a WaveEngine preview version

---
+++ WaveEngine.MRTK/SDK/Vector3Extensions.cs
@@ -0,0 +1,21 @@
+﻿using WaveEngine.Mathematics;
+
+namespace WaveEngine.MRTK.SDK
+{
+    public class Vector3Extensions
+    {
+        public static Vector3 Project(Vector3 vector, Vector3 onVector)
+        {
+            var lengthSquared = onVector.LengthSquared();
+
+            if (MathHelper.FloatEquals(lengthSquared, 0.0f))
+            {
+                return Vector3.Zero;
+            }
+            else
+            {
+                return onVector * Vector3.Dot(vector, onVector) / lengthSquared;
+            }
+        }
+    }
+}
