08054a6916104ba616e0b697467b800f88b2e47f
Manuel Caballero Sánchez <minolo3ds@gmail.com>
2020-03-17 10:48:15 +0100

    Simplified NoesisServiceRegistrationComponent

--- Samples/WaveEngine_MRTK_Demo/WaveEngine_MRTK_Demo/Components/NoesisServiceRegistrationComponent.cs
+++ Samples/WaveEngine_MRTK_Demo/WaveEngine_MRTK_Demo/Components/NoesisServiceRegistrationComponent.cs
@@ -1,63 +1,30 @@
-﻿using WaveEngine.Common.Attributes;
-using WaveEngine.Framework;
+﻿using WaveEngine.Framework;
 using WaveEngine.NoesisGUI;
 
 namespace WaveEngine_MRTK_Demo.Components
 {
     public class NoesisServiceRegistrationComponent : Component
     {
-        [WaveIgnore]
-        public NoesisService NoesisService;
-
-        private string noesisStyle = null;
-
-        public string NoesisStyle
-        {
-            get => this.noesisStyle;
-            set
-            {
-                this.noesisStyle = value;
-                if (this.NoesisService != null)
-                {
-                    this.NoesisService.Style = value;
-                }
-            }
-        }
-
         protected override void OnLoaded()
         {
             base.OnLoaded();
 
-            this.NoesisService = Application.Current.Container.Resolve<NoesisService>();
-            if (this.NoesisService == null)
+            var noesisService = Application.Current.Container.Resolve<NoesisService>();
+            if (noesisService == null)
             {
-                this.NoesisService = new NoesisService()
+                noesisService = new NoesisService()
                 {
-                    Style = this.noesisStyle,
-                    IsEnabled = this.IsEnabled
+                    Style = null,
                 };
 
-                Application.Current.Container.RegisterInstance(this.NoesisService);
+                Application.Current.Container.RegisterInstance(noesisService);
             }
-        }
-
-        protected override void OnActivated()
-        {
-            base.OnActivated();
-            this.NoesisService.IsEnabled = true;
-        }
-
-        protected override void OnDeactivated()
-        {
-            base.OnDeactivated();
-            // this.NoesisService.IsEnabled = false;
         }
 
         protected override void OnDestroy()
         {
             base.OnDestroy();
             Application.Current.Container.Unregister<NoesisService>();
-            this.NoesisService = null;
         }
     }
 }
