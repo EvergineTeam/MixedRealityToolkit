8b50ed4caa69de77f515a3171b7caf49ad3da2aa
gsantos <gsantos@plainconcepts.com>
2020-03-18 11:24:22 +0100

    hand automatically hidden when out of view

--- Samples/WaveEngine_MRTK_Demo/WaveEngine_MRTK_Demo/Behaviors/HoloHandsUpdater.cs
+++ Samples/WaveEngine_MRTK_Demo/WaveEngine_MRTK_Demo/Behaviors/HoloHandsUpdater.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using WaveEngine.Framework;
 using WaveEngine.Framework.Graphics;
+using WaveEngine.Mathematics;
 using WaveEngine_MRTK_Demo.Effects;
 
 namespace WaveEngine_MRTK_Demo.Behaviors
@@ -14,12 +15,15 @@ namespace WaveEngine_MRTK_Demo.Behaviors
 
         public Material mat;
 
-        HoloHands holoHandsDecorator;
+        private HoloHands holoHandsDecorator;
+        private Camera3D camera;
 
         protected override bool OnAttached()
         {
             holoHandsDecorator = new HoloHands(mat);
             mat.ActiveDirectivesNames = new string[] { "MULTIVIEW", "PULSE" };
+
+            camera = this.Managers.RenderManager.ActiveCamera3D;
 
             return base.OnAttached();
         }
@@ -28,7 +32,15 @@ namespace WaveEngine_MRTK_Demo.Behaviors
         protected override void Update(TimeSpan gameTime)
         {
             time += (float)gameTime.TotalSeconds;
-            holoHandsDecorator.Matrices_TPosY = transform.Position.Y - ((float)Math.Sin(time) + 1.0f) * 0.1f;
+            holoHandsDecorator.Matrices_TPosY = transform.Position.Y  + 0.1f - time * 0.2f;
+
+            Vector3 camForward = camera.Transform.WorldTransform.Forward;
+            Vector3 camPosV = transform.Position - camera.Transform.Position;
+
+            if (Vector3.Angle(camForward, camPosV) > (0.3f))
+            {
+                time = 0;
+            }
         }
     }
 }
