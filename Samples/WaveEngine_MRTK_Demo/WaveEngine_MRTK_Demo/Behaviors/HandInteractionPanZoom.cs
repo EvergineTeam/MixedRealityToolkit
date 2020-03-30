cbff0ba1b014859605ae74112713a7939bc5cbb0
gsantos <gsantos@plainconcepts.com>
2020-03-30 15:52:19 +0200

    HandInteractionPanZoom with pan working

---
+++ Samples/WaveEngine_MRTK_Demo/WaveEngine_MRTK_Demo/Behaviors/HandInteractionPanZoom.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WaveEngine.Components.Graphics3D;
+using WaveEngine.Framework;
+using WaveEngine.Framework.Graphics;
+using WaveEngine.Mathematics;
+using WaveEngine.MRTK.Base.EventDatum.Input;
+using WaveEngine.MRTK.Base.Interfaces.InputSystem.Handlers;
+using WaveEngine.MRTK.SDK.Features.UX.Components.PressableButtons;
+using WaveEngine.MRTK.Services.InputSystem;
+using WaveEngine_MRTK_Demo.Effects;
+using WaveEngine_MRTK_Demo.Emulation;
+
+namespace WaveEngine_MRTK_Demo.Behaviors
+{
+    public class HandInteractionPanZoom : Behavior, IMixedRealityTouchHandler
+    {
+        [BindComponent]
+        protected Transform3D transform = null;
+
+        [BindComponent(isRequired: true, source: BindComponentSource.Owner)]
+        protected MaterialComponent materialComponent;
+
+        [BindComponent]
+        protected NearInteractionTouchable nearInteractionTouchable;
+
+        private Slate slateDecorator;
+        private CursorManager cursorManager;
+
+        class TouchInfo
+        {
+            public Vector2 uv; // uv when the collision started
+            public Entity cursor;
+            public Transform3D transform;
+        }
+
+        private List<TouchInfo> touchInfos = new List<TouchInfo>();
+
+        protected override void Start()
+        {
+            slateDecorator = new Slate(materialComponent.Material);
+            cursorManager = Owner.Scene.Managers.FindManager<CursorManager>();
+        }
+
+        private Vector2 GetUVPos(Vector3 position)
+        {
+            var matrix = this.transform.WorldInverseTransform * this.nearInteractionTouchable.BoxCollider3DTransformInverse;
+            Vector3 localPos =  Vector3.TransformCoordinate(position, matrix);
+
+            Vector2 uv0 = slateDecorator.Matrices_Offset * slateDecorator.Matrices_Tiling;
+            Vector2 uv1 = uv0 + Vector2.One * slateDecorator.Matrices_Tiling;
+
+            Vector2 t = new Vector2(localPos.X + 0.5f, localPos.Z + 0.5f);
+
+            return new Vector2(
+                MathHelper.Lerp(uv0.X, uv1.X, t.X),
+                MathHelper.Lerp(uv0.Y, uv1.Y, t.Y)
+            );
+        }
+
+        protected override void Update(TimeSpan gameTime)
+        {
+            if (touchInfos.Count > 0)
+            {
+                //Scale
+                if(touchInfos.Count > 1)
+                {
+
+                }
+
+                //Translate
+                Vector2 uv = GetUVPos(touchInfos[0].transform.Position);
+                Vector2 disp = uv - touchInfos[0].uv;
+                slateDecorator.Matrices_Offset -= disp * slateDecorator.Matrices_Tiling;
+            }
+        }
+
+        public void OnTouchStarted(HandTrackingInputEventData eventData)
+        {
+            touchInfos.Add(new TouchInfo { cursor = eventData.Cursor, transform = eventData.Cursor.FindComponent< Transform3D >(), uv = GetUVPos(eventData.Position)});
+        }
+
+        public void OnTouchUpdated(HandTrackingInputEventData eventData)
+        {
+        }
+
+        public void OnTouchCompleted(HandTrackingInputEventData eventData)
+        {
+            var node = touchInfos[0];
+            
+            for(int i = 0; i < touchInfos.Count; ++ i)
+            {
+                if (touchInfos[i] .cursor == eventData.Cursor)
+                {
+                    touchInfos.RemoveAt(i);
+                    break;
+                }
+            }
+        }
+    }
+}
