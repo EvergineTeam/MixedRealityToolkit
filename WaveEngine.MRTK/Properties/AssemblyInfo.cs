e16c4e1acd3113335fd6e1d1d0a0353ac8a05570
Manuel Caballero Sánchez <minolo3ds@gmail.com>
2020-03-31 16:49:58 +0200

    Removed year from header

--- WaveEngine.MRTK/Properties/AssemblyInfo.cs
+++ WaveEngine.MRTK/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿// Copyright © 2019 Wave Engine S.L. All rights reserved. Use is subject to license terms.
+﻿// Copyright © Wave Engine S.L. All rights reserved. Use is subject to license terms.
 
 using WaveEngine.Common.Attributes;
 
