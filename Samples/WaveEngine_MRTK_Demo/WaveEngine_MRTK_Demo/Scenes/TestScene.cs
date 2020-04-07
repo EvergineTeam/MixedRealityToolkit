0a5391c55fec2b57f1514c8a66de3e6048424ea8
Manuel Caballero Sánchez <minolo3ds@gmail.com>
2020-04-07 13:13:04 +0200

    Updated TestScene to use the new way of using cursors

--- Samples/WaveEngine_MRTK_Demo/WaveEngine_MRTK_Demo/Scenes/TestScene.cs
+++ Samples/WaveEngine_MRTK_Demo/WaveEngine_MRTK_Demo/Scenes/TestScene.cs
@@ -31,8 +31,26 @@ namespace WaveEngine_MRTK_Demo.Scenes
             if (xrPlatform != null)
             {
                 // HoloLens 2
-                cursorLeftEntity.AddComponent(new TrackXRJoint() { Handedness = XRHandedness.LeftHand, SelectionStrategy = TrackXRDevice.SelectionDeviceStrategy.ByHandedness, JointKind = XRHandJointKind.IndexTip });
-                cursorRightEntity.AddComponent(new TrackXRJoint() { Handedness = XRHandedness.RightHand, SelectionStrategy = TrackXRDevice.SelectionDeviceStrategy.ByHandedness, JointKind = XRHandJointKind.IndexTip });
+                cursorLeftEntity?
+                    .AddComponent(new TrackXRJoint()
+                    {
+                        Handedness = XRHandedness.LeftHand,
+                        SelectionStrategy = TrackXRDevice.SelectionDeviceStrategy.ByHandedness,
+                        JointKind = XRHandJointKind.IndexTip,
+                        TrackingLostMode = TrackXRDevice.XRTrackingLostMode.KeepLastPose,
+                    })
+                    .AddComponent(new HoloLensControlBehavior())
+                    ;
+                cursorRightEntity?
+                    .AddComponent(new TrackXRJoint()
+                    {
+                        Handedness = XRHandedness.RightHand,
+                        SelectionStrategy = TrackXRDevice.SelectionDeviceStrategy.ByHandedness,
+                        JointKind = XRHandJointKind.IndexTip,
+                        TrackingLostMode = TrackXRDevice.XRTrackingLostMode.KeepLastPose,
+                    })
+                    .AddComponent(new HoloLensControlBehavior())
+                    ;
             }
             else
             {
