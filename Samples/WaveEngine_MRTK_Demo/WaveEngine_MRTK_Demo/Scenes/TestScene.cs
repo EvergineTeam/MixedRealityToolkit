dab50a97cc9a09f704957e822232e485d05a45bb
gsantos <gsantos@plainconcepts.com>
2020-08-05 10:56:50 +0200

    Moving MRTK base resources to a separate folder

--- Samples/WaveEngine_MRTK_Demo/WaveEngine_MRTK_Demo/Scenes/TestScene.cs
+++ Samples/WaveEngine_MRTK_Demo/WaveEngine_MRTK_Demo/Scenes/TestScene.cs
@@ -5,18 +5,18 @@ namespace WaveEngine_MRTK_Demo.Scenes
 {
     public class TestScene : XRScene
     {
-        protected override Guid CursorMatPressed => WaveContent.Materials.CursorLeftPinch;
+        protected override Guid CursorMatPressed => WaveContent.MRTK.Materials.CursorLeftPinch;
 
-        protected override Guid CursorMatReleased => WaveContent.Materials.CursorLeft;
+        protected override Guid CursorMatReleased => WaveContent.MRTK.Materials.CursorLeft;
 
-        protected override Guid HoloHandsMat => WaveContent.Materials.HoloHands;
+        protected override Guid HoloHandsMat => WaveContent.MRTK.Materials.HoloHands;
 
-        protected override Guid HolographicEffect => WaveContent.Effects.HoloGraphic;
+        protected override Guid HolographicEffect => WaveContent.MRTK.Effects.HoloGraphic;
 
         protected override Guid SpatialMappingMat => Guid.Empty;
 
-        protected override Guid HandRayTexture => WaveContent.Textures.line_dots_png;
+        protected override Guid HandRayTexture => WaveContent.MRTK.Textures.line_dots_png;
 
-        protected override Guid HandRaySampler => WaveContent.Samplers.LinearWrapSampler;
+        protected override Guid HandRaySampler => WaveContent.MRTK.Samplers.LinearWrapSampler;
     }
 }
