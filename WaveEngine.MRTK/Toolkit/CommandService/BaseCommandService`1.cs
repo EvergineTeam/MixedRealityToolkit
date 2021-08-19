c6ec3ae52b3dfd7fe9c176f72517333d9baacf14
Manuel Caballero Sánchez <minolo3ds@gmail.com>
2021-08-19 12:51:55 +0200

    Refactored the CommandService to include optional event handling mark and handled event.

--- WaveEngine.MRTK/Toolkit/CommandService/BaseCommandService`1.cs
+++ WaveEngine.MRTK/Toolkit/CommandService/BaseCommandService`1.cs
@@ -1,6 +1,8 @@
 ﻿// Copyright © Wave Engine S.L. All rights reserved. Use is subject to license terms.
 
 using System;
+using System.Collections.Generic;
+using WaveEngine.Framework;
 using WaveEngine.Framework.Services;
 
 namespace WaveEngine.MRTK.Toolkit.CommandService
@@ -12,19 +14,48 @@ namespace WaveEngine.MRTK.Toolkit.CommandService
     public abstract class BaseCommandService<T> : Service
         where T : Enum
     {
-        /// <summary>
-        /// Event raised when a new command is received.
-        /// </summary>
-        public event EventHandler<CommandEventArgs> NewCommandReceived;
+        private List<EventHandler<CommandReceivedEventArgs>> CommandReceivedEventHandlers = new List<EventHandler<CommandReceivedEventArgs>>();
 
         /// <summary>
-        /// Send a new command request.
+        /// Event raised when a command is received.
         /// </summary>
+        public event EventHandler<CommandReceivedEventArgs> CommandReceived
+        {
+            add => this.CommandReceivedEventHandlers.Add(value);
+            remove => this.CommandReceivedEventHandlers.Remove(value);
+        }
+
+        /// <summary>
+        /// Event raised when a command is handled.
+        /// </summary>
+        public event EventHandler<CommandHandledEventArgs> CommandHandled;
+
+        /// <summary>
+        /// Execute a command request.
+        /// </summary>
+        /// <param name="source">The component that requested the command.</param>
         /// <param name="command">The command to request.</param>
         /// <param name="parameter">The parameter for the request.</param>
-        public void SendCommand(T command, object parameter = null)
+        /// <returns>
+        /// <see langword="true"/> if the command was handled; otherwise, <see langword="false"/>.
+        /// </returns>
+        public bool ExecuteCommand(Component source, T command, object[] parameter = null)
         {
-            this.NewCommandReceived?.Invoke(this, new CommandEventArgs(command, parameter));
+            var args = new CommandReceivedEventArgs(source, command, parameter);
+
+            foreach (var handler in this.CommandReceivedEventHandlers)
+            {
+                handler.Invoke(this, args);
+
+                if (args.Handled)
+                {
+                    var handledArgs = new CommandHandledEventArgs(handler.Target, command);
+                    this.CommandHandled?.Invoke(this, handledArgs);
+                    break;
+                }
+            }
+
+            return args.Handled;
         }
     }
 }
---
+++ WaveEngine.MRTK/Toolkit/CommandService/CommandReceivedEventArgs.cs
@@ -0,0 +1,54 @@
+﻿// Copyright © Wave Engine S.L. All rights reserved. Use is subject to license terms.
+
+using System;
+using WaveEngine.Framework;
+
+namespace WaveEngine.MRTK.Toolkit.CommandService
+{
+    /// <summary>
+    /// Event arguments received when a command is requested.
+    /// </summary>
+    public class CommandReceivedEventArgs : EventArgs
+    {
+        /// <summary>
+        /// Gets the <see cref="Component"/> that made the request.
+        /// </summary>
+        public Component Source { get; private set; }
+
+        /// <summary>
+        /// Gets the requested command.
+        /// </summary>
+        public Enum Command { get; private set; }
+
+        /// <summary>
+        /// Gets the parameters for the request.
+        /// </summary>
+        public object[] Parameter { get; private set; }
+
+        /// <summary>
+        /// Gets a value indicating whether this event has been marked as handled.
+        /// </summary>
+        public bool Handled { get; private set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CommandReceivedEventArgs"/> class.
+        /// </summary>
+        /// <param name="source">The component that requested the command.</param>
+        /// <param name="command">The requested command.</param>
+        /// <param name="parameter">The parameter for the request.</param>
+        public CommandReceivedEventArgs(Component source, Enum command, object[] parameter)
+        {
+            this.Source = source;
+            this.Command = command;
+            this.Parameter = parameter;
+        }
+
+        /// <summary>
+        /// Mark the event as handled.
+        /// </summary>
+        public void SetHandled()
+        {
+            this.Handled = true;
+        }
+    }
+}
