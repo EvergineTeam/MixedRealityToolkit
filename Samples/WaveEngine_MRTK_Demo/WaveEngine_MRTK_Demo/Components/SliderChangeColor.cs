46d4dd7a822e1ceb49cb8bf00bfffa52a4b630ff
Manuel Caballero Sánchez <minolo3ds@gmail.com>
2021-09-14 13:15:19 +0200

    Added slider configurators to the demo scene

--- Samples/WaveEngine_MRTK_Demo/WaveEngine_MRTK_Demo/Components/SliderChangeColor.cs
+++ Samples/WaveEngine_MRTK_Demo/WaveEngine_MRTK_Demo/Components/SliderChangeColor.cs
@@ -11,68 +11,64 @@ namespace WaveEngine_MRTK_Demo.Components
     class SliderChangeColor : Component
     {
         [BindComponent(isRequired: true, source: BindComponentSource.Scene, tag: "PinchSliderRed")]
-        protected ScenePrefab pinchSliderPrefabR = null;
+        private ScenePrefab pinchSliderPrefabR = null;
 
         [BindComponent(isRequired: true, source: BindComponentSource.Scene, tag: "PinchSliderGreen")]
-        protected ScenePrefab pinchSliderPrefabG = null;
+        private ScenePrefab pinchSliderPrefabG = null;
 
         [BindComponent(isRequired: true, source: BindComponentSource.Scene, tag: "PinchSliderBlue")]
-        protected ScenePrefab pinchSliderPrefabB = null;
+        private ScenePrefab pinchSliderPrefabB = null;
 
         [BindComponent(isRequired: true, source: BindComponentSource.Owner)]
-        protected MaterialComponent materialComponent = null;
+        private MaterialComponent materialComponent = null;
 
         private PinchSlider[] pinchSliders = new PinchSlider[3];
         private HoloGraphic materialDecorator;
 
         protected override void Start()
         {
-            if (Application.Current.IsEditor) return;
+            if (Application.Current.IsEditor)
+            {
+                return;
+            }
 
-            materialDecorator = new HoloGraphic(materialComponent.Material);
+            this.materialDecorator = new HoloGraphic(materialComponent.Material);
 
-            this.pinchSliders[0] = pinchSliderPrefabR.Owner.FindComponentInChildren<PinchSlider>();
-            this.pinchSliders[1] = pinchSliderPrefabG.Owner.FindComponentInChildren<PinchSlider>();
-            this.pinchSliders[2] = pinchSliderPrefabB.Owner.FindComponentInChildren<PinchSlider>();
+            this.pinchSliders[0] = this.pinchSliderPrefabR.Owner.FindComponentInChildren<PinchSlider>();
+            this.pinchSliders[1] = this.pinchSliderPrefabG.Owner.FindComponentInChildren<PinchSlider>();
+            this.pinchSliders[2] = this.pinchSliderPrefabB.Owner.FindComponentInChildren<PinchSlider>();
 
-            string[] titles = {"Red", "Green", "Blue"};
             for (int i = 0; i < 3; ++i)
             {
-                PinchSlider p = pinchSliders[i];
-                Entity title = p.Owner.FindChild("Title", true);
-                if (title != null)
-                {
-                    Text3D text = title.FindComponent<Text3D>();
-                    if (text != null)
-                    {
-                        text.Text = titles[i];
-                    }
-                }
+                var p = pinchSliders[i];
 
-                p.InitialValue = materialDecorator.Parameters_Color[i];
-                p.SliderValue = materialDecorator.Parameters_Color[i];
-                p.ValueUpdated += this.P_ValueUpdated;
+                p.InitialValue = this.materialDecorator.Parameters_Color[i];
+                p.SliderValue = this.materialDecorator.Parameters_Color[i];
+                p.ValueUpdated += this.PinchSlider_ValueUpdated;
             }
         }
 
         protected override void OnDestroy()
         {
-            if (Application.Current.IsEditor) return;
+            if (Application.Current.IsEditor)
+            {
+                return;
+            }
 
             for (int i = 0; i < 3; ++i)
             {
-                this.pinchSliders[i].ValueUpdated -= this.P_ValueUpdated;
+                this.pinchSliders[i].ValueUpdated -= this.PinchSlider_ValueUpdated;
             }
         }
 
-        private void P_ValueUpdated(object sender, SliderEventData e)
+        private void PinchSlider_ValueUpdated(object sender, SliderEventData e)
         {
             for (int i = 0; i < this.pinchSliders.Length; ++i)
             {
                 if (sender == this.pinchSliders[i])
                 {
-                    Vector3 c = this.materialDecorator.Parameters_Color;
-                    c[i] = (e.NewValue);
+                    var c = this.materialDecorator.Parameters_Color;
+                    c[i] = e.NewValue;
                     this.materialDecorator.Parameters_Color = c;
 
                     break;
