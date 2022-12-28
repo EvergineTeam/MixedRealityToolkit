2f6e2e3a3906fa2fdee89f577f21bfa3c1282160
Martin Šulc <martin.sulc@pocketvirtuality.com>
2022-12-28 13:57:55 +0100

    #6 - Fix ArgumentNullException AssetsService.Load

--- Samples/Evergine.MRTK.Demo/Evergine.MRTK.Demo/MyApplication.cs
+++ Samples/Evergine.MRTK.Demo/Evergine.MRTK.Demo/MyApplication.cs
@@ -13,39 +13,39 @@ namespace Evergine.MRTK.Demo
     {
         public MyApplication()
         {
-            this.Container.RegisterType<Clock>();
-            this.Container.RegisterType<TimerFactory>();
-            this.Container.RegisterType<Random>();
-            this.Container.RegisterType<ErrorHandler>();
-            this.Container.RegisterType<ScreenContextManager>();
-            this.Container.RegisterType<GraphicsPresenter>();
-            this.Container.RegisterType<AssetsDirectory>();
-            this.Container.RegisterType<AssetsService>();
-            this.Container.RegisterType<ForegroundTaskSchedulerService>();            
-            this.Container.RegisterType<WorkActionScheduler>();
+            Container.RegisterType<Clock>();
+            Container.RegisterType<TimerFactory>();
+            Container.RegisterType<Random>();
+            Container.RegisterType<ErrorHandler>();
+            Container.RegisterType<ScreenContextManager>();
+            Container.RegisterType<GraphicsPresenter>();
+            Container.RegisterType<AssetsDirectory>();
+            Container.RegisterType<AssetsService>();
+            Container.RegisterType<ForegroundTaskSchedulerService>();            
+            Container.RegisterType<WorkActionScheduler>();
 
-            this.Container.RegisterType<DemoCommandService>();
+            Container.RegisterType<DemoCommandService>();
 
-            ForegroundTaskScheduler.Foreground.Configure(this.Container);
-            BackgroundTaskScheduler.Background.Configure(this.Container);
+            ForegroundTaskScheduler.Foreground.Configure(Container);
+            BackgroundTaskScheduler.Background.Configure(Container);
         }
 
         public override void Initialize()
         {
             base.Initialize();
 
-            var voiceCommandService = this.Container.Resolve<IVoiceCommandService>();
+            var voiceCommandService = Container.Resolve<IVoiceCommandService>();
             if (voiceCommandService == null)
             {
                 voiceCommandService = new FakeVoiceCommandService();
-                this.Container.RegisterInstance(voiceCommandService);
+                Container.RegisterInstance(voiceCommandService);
             }
 
             voiceCommandService.ConfigureVoiceCommands(VoiceKeywords.ValidVoiceKeywords);
 
             // Get ScreenContextManager
-            var screenContextManager = this.Container.Resolve<ScreenContextManager>();
-            var assetsService = this.Container.Resolve<AssetsService>();
+            var screenContextManager = Container.Resolve<ScreenContextManager>();
+            var assetsService = Container.Resolve<AssetsService>();
 
             // Navigate to scene
             var scene = assetsService.Load<DemoScene>(EvergineContent.Scenes.DemoScene_wescene);
