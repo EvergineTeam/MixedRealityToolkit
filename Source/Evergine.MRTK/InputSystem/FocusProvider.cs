6fde45bfa71af5ea834c665097213c252bd4a599
Manuel Caballero Sánchez <minolo3ds@gmail.com>
2023-03-29 14:05:38 +0200

    Register pointers and implement basic update loop

--- Source/Evergine.MRTK/InputSystem/FocusProvider.cs
+++ Source/Evergine.MRTK/InputSystem/FocusProvider.cs
@@ -1,7 +1,10 @@
 ﻿// Copyright © Evergine S.L. All rights reserved. Use is subject to license terms.
 
 using Evergine.Framework.Managers;
+using Evergine.MRTK.InputSystem.Pointers;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Evergine.MRTK.InputSystem
 {
@@ -10,10 +13,48 @@ namespace Evergine.MRTK.InputSystem
     /// </summary>
     public class FocusProvider : UpdatableSceneManager
     {
+        private List<PointerData> pointers = new List<PointerData>();
+
         /// <inheritdoc/>
         public override void Update(TimeSpan gameTime)
         {
-            ////throw new NotImplementedException();
+            foreach (var pointer in this.pointers)
+            {
+                pointer.Pointer.UpdateInteractions();
+            }
+        }
+
+        /// <summary>
+        /// Register a pointer into the <see cref="FocusProvider"/>.
+        /// </summary>
+        /// <param name="pointer">The pointer to be registered.</param>
+        public void RegisterPointer(BasePointer pointer)
+        {
+            if (!this.pointers.Any(p => p.Pointer == pointer))
+            {
+                this.pointers.Add(new PointerData()
+                {
+                    Pointer = pointer,
+                });
+            }
+        }
+
+        /// <summary>
+        /// Unregister a pointer from the <see cref="FocusProvider"/>.
+        /// </summary>
+        /// <param name="pointer">The pointer to be unregistered.</param>
+        public void UnregisterPointer(BasePointer pointer)
+        {
+            var pointerData = this.pointers.FirstOrDefault(p => p.Pointer == pointer);
+            if (pointerData != null)
+            {
+                this.pointers.Remove(pointerData);
+            }
+        }
+
+        private class PointerData
+        {
+            public BasePointer Pointer { get; set; }
         }
     }
 }
--- Source/Evergine.MRTK/InputSystem/Pointers/BasePointer.cs
+++ Source/Evergine.MRTK/InputSystem/Pointers/BasePointer.cs
@@ -25,11 +25,35 @@ namespace Evergine.MRTK.InputSystem.Pointers
         /// </summary>
         public virtual CollisionCategory3D CollisionCategoryMask { get; protected set; } = CollisionCategory3D.All;
 
+        [BindSceneManager]
+        private FocusProvider focusProvider = null;
+
         /// <summary>
         /// The <see cref="BaseXRController"/> component.
         /// </summary>
         [BindComponent(source: BindComponentSource.Parents, isExactType: false)]
         public BaseXRController XRController = null;
+
+        /// <inheritdoc/>
+        protected override bool OnAttached()
+        {
+            if (!base.OnAttached())
+            {
+                return false;
+            }
+
+            this.focusProvider.RegisterPointer(this);
+
+            return true;
+        }
+
+        /// <inheritdoc/>
+        protected override void OnDetach()
+        {
+            base.OnDetach();
+
+            this.focusProvider.UnregisterPointer(this);
+        }
 
         /// <summary>
         /// Update all interactions that this pointer has with the rest of objects on the scene.
