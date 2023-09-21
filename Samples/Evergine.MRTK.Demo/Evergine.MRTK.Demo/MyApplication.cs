30d6d81fe506913d34c2dab6df73f1e6d93b4bd9
Sergio Escalada Beltrán <sescalada@plainconcepts.com>
2023-09-21 18:41:33 +0200

    Update to 2023.9.21.1-nightly

--- Samples/Evergine.MRTK.Demo/Evergine.MRTK.Demo/MyApplication.cs
+++ Samples/Evergine.MRTK.Demo/Evergine.MRTK.Demo/MyApplication.cs
@@ -13,18 +13,18 @@ namespace Evergine.MRTK.Demo
     {
         public MyApplication()
         {
-            this.Container.RegisterType<Clock>();
-            this.Container.RegisterType<TimerFactory>();
-            this.Container.RegisterType<Random>();
-            this.Container.RegisterType<ErrorHandler>();
-            this.Container.RegisterType<ScreenContextManager>();
-            this.Container.RegisterType<GraphicsPresenter>();
-            this.Container.RegisterType<AssetsDirectory>();
-            this.Container.RegisterType<AssetsService>();
-            this.Container.RegisterType<ForegroundTaskSchedulerService>();            
-            this.Container.RegisterType<WorkActionScheduler>();
+            this.Container.Register<Clock>();
+            this.Container.Register<TimerFactory>();
+            this.Container.Register<Random>();
+            this.Container.Register<ErrorHandler>();
+            this.Container.Register<ScreenContextManager>();
+            this.Container.Register<GraphicsPresenter>();
+            this.Container.Register<AssetsDirectory>();
+            this.Container.Register<AssetsService>();
+            this.Container.Register<ForegroundTaskSchedulerService>();            
+            this.Container.Register<WorkActionScheduler>();
 
-            this.Container.RegisterType<DemoCommandService>();
+            this.Container.Register<DemoCommandService>();
 
             ForegroundTaskScheduler.Foreground.Configure(this.Container);
             BackgroundTaskScheduler.Background.Configure(this.Container);
