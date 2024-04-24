b85d93c1c8950973d51f89448ef13973fa554e75
Rafael Caro <rafael.caro@plainconcepts.com>
2024-04-24 16:20:42 +0200

    feat: teleport line prefab and teleport ray wip

--- Source/Evergine.MRTK/Emulation/TeleportRay.cs
+++ Source/Evergine.MRTK/Emulation/TeleportRay.cs
@@ -1,10 +1,13 @@
 ﻿// Copyright © Evergine S.L. All rights reserved. Use is subject to license terms.
+using Evergine.Common.Attributes;
+using Evergine.Common.Input;
+using Evergine.Components.XR;
 using Evergine.Framework;
-using Evergine.Framework.Graphics;
 using Evergine.Framework.Physics3D;
+using Evergine.Framework.Prefabs;
 using Evergine.Framework.Threading;
+using Evergine.Mathematics;
 using System;
-using System.Linq;
 
 namespace Evergine.MRTK.Emulation
 {
@@ -14,10 +17,44 @@ namespace Evergine.MRTK.Emulation
     public class TeleportRay : Behavior
     {
         //// Teleport Effect Area
-        private Entity teleportAreaEntity = null;
-        private StaticBody3D teleportAreaStaticBody = null;
-        private SphereCollider3D sphereCollider = null;
-        private Transform3D teleportAreaTransform = null;
+        ////private Entity teleportAreaEntity = null;
+        ////private StaticBody3D teleportAreaStaticBody = null;
+        ////private SphereCollider3D sphereCollider = null;
+        ////private Transform3D teleportAreaTransform = null;
+
+        /// <summary>
+        /// Prefab for teleportation area.
+        /// </summary>
+        [RenderProperty(CustomPropertyName = "Teleport Area", Tooltip = "The teleport area prefab to be instantiated")]
+        public Prefab TeleportationAreaPrefab = null;
+
+        /// <summary>
+        /// Prefab for teleportation line.
+        /// </summary>
+        [RenderProperty(CustomPropertyName = "Teleport Area", Tooltip = "The teleport line prefab to be instantiated")]
+        public Prefab TeleportationLinePrefab = null;
+
+        /// <summary>
+        /// Gets or sets the gravity to be applied to the pointer arc trace.
+        /// </summary>
+        [RenderProperty(CustomPropertyName = "Pointer Gravity", Tooltip = "The gravity to be applied to the pointer arc trace")]
+        public float Gravity { get; set; } = -9.8f;
+
+        /// <summary>
+        /// Controller to track with ray.
+        /// </summary>
+        public TrackXRController controller = null;
+
+        //// Prefab entities
+        private Entity teleportationAreaEntity = null;
+        private Entity teleportationLineEntity = null;
+
+        //// Collisions
+        ////private bool detectedCollisionArea = false;
+        ////private bool groundDetected = false;
+        ////private bool rayCollisionDetected = false;
+
+        private bool isManipulationActive = false;
 
         /// <inheritdoc/>
         protected override async void Start()
@@ -30,31 +67,90 @@ namespace Evergine.MRTK.Emulation
 
             await EvergineForegroundTask.Run(() =>
             {
-                this.teleportAreaEntity = new Entity();
-                this.teleportAreaTransform = new Transform3D();
-                this.teleportAreaStaticBody = new StaticBody3D()
+                this.teleportationAreaEntity = this.TeleportationAreaPrefab.Instantiate();
+                if (this.teleportationAreaEntity != null)
                 {
-                    IsSensor = true,
-                    MaskBits = CollisionCategory3D.Cat1,
-                    CollisionCategories = CollisionCategory3D.Cat3,
-                };
-                this.sphereCollider = new SphereCollider3D()
+                    this.teleportationAreaEntity.IsEnabled = false;
+                    this.Managers.EntityManager.Add(this.teleportationAreaEntity);
+                }
+
+                this.teleportationLineEntity = this.TeleportationLinePrefab.Instantiate();
+                if (this.teleportationLineEntity != null)
                 {
-                    Radius = 0.4f,
-                    Margin = 0.040f,
-                    Offset = new Mathematics.Vector3(0.0f, -0.5f, 0.0f),
-                };
-                this.teleportAreaEntity
-                    .AddComponent(this.teleportAreaTransform)
-                    .AddComponent(this.sphereCollider)
-                    .AddComponent(this.teleportAreaStaticBody);
+                    this.Managers.EntityManager.Add(this.teleportationAreaEntity);
+                }
+
+                var teleportationAreaStaticBody = this.teleportationAreaEntity.FindComponent<StaticBody3D>();
+                if (teleportationAreaStaticBody != null)
+                {
+                    teleportationAreaStaticBody.BeginCollision += this.TeleportationAreaStaticBodyBeginCollision;
+                    teleportationAreaStaticBody.EndCollision += this.TeleportationAreaStaticBodyEndCollision;
+                }
             });
+        }
+
+        private void TeleportationAreaStaticBodyEndCollision(object sender, CollisionInfo3D e)
+        {
+            ////this.detectedCollisionArea = false;
+        }
+
+        private void TeleportationAreaStaticBodyBeginCollision(object sender, CollisionInfo3D e)
+        {
+            ////this.detectedCollisionArea = true;
         }
 
         /// <inheritdoc/>
         protected override void Update(TimeSpan gameTime)
         {
+            if (this.controller == null)
+            {
+                return;
+            }
+
+            if (!this.isManipulationActive && !this.IsCursorCollision())
+            {
+                var btnState = this.controller.ControllerState.TriggerButton;
+                if (btnState == ButtonState.Pressed)
+                {
+                    this.PressedBehavior();
+                }
+                else if (btnState == ButtonState.Releasing)
+                {
+                    this.ReleasingBehavior();
+                }
+            }
+            else
+            {
+                this.teleportationAreaEntity.IsEnabled = false;
+                this.DeleteLineMesh();
+            }
+        }
+
+        private void DeleteLineMesh()
+        {
             throw new NotImplementedException();
+        }
+
+        private void ReleasingBehavior()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void PressedBehavior()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Checks if there is something too close to the cursor (the user could be trying to grab something).
+        /// </summary>
+        /// <returns>true if the user hand cursor is close to something.</returns>
+        private bool IsCursorCollision()
+        {
+            Ray ray = this.controller.Pointer;
+            var collisionResult = this.Managers.PhysicManager3D.RayCast(ref ray, 0.2f, CollisionCategory3D.All);
+
+            return collisionResult.Succeeded;
         }
     }
 }
